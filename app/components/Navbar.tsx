b35f0c1f2682fe27d3f14231275996fdfbeafbeb
benthecoder <benthecoder07@gmail.com>
2023-12-03 19:17:53 -0600

    Add Link paths to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,35 +14,41 @@ import {
   PiCalendar,
   PiGear,
 } from 'react-icons/pi';
+import Link from 'next/link';
 
 import { UserButton } from '@clerk/nextjs';
 
-const Menu = ({ label, icon }) => {
+const Menu = ({ label, icon, path }) => {
+  if (path == null) {
+    path = '/';
+  }
   return (
     <Tooltip label={label} placement='right'>
-      <IconButton
-        aria-label={label}
-        icon={icon}
-        variant='ghost'
-        fontSize='30px'
-        background='gray.100'
-        _hover={{ bg: 'blue.100' }}
-      />
+      <Link href={path}>
+        <IconButton
+          aria-label={label}
+          icon={icon}
+          variant='ghost'
+          fontSize='28px'
+          className='bg-gray-200'
+          _hover={{ bg: 'gray.500' }}
+        />
+      </Link>
     </Tooltip>
   );
 };
 
 export default function Navbar() {
   return (
-    <VStack spacing='10' left='0' p='5' h='100%'>
-      <Text fontSize='xl' marginBottom={20}>
+    <VStack spacing='10' left='0' p='5' h='100%' shadow='xl'>
+      <Text fontSize='xl' marginBottom={20} fontFamily='serif' fontStyle='bold'>
         Cuepal
       </Text>
-      <Menu label='Home' icon={<PiHouse />} />
-      <Menu label='Class' icon={<PiGraduationCap />} />
-      <Menu label='Cards' icon={<PiCards />} />
-      <Menu label='Calendar' icon={<PiCalendar />} />
-      <Menu label='Settings' icon={<PiGear />} />
+      <Menu label='Home' icon={<PiHouse />} path='/' />
+      <Menu label='Class' icon={<PiGraduationCap />} path='/course' />
+      <Menu label='Cards' icon={<PiCards />} path='/cards' />
+      <Menu label='Calendar' icon={<PiCalendar />} path='/calendar' />
+      <Menu label='Settings' icon={<PiGear />} path='/settings' />
       <Spacer />
       <UserButton />
     </VStack>
