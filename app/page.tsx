fea1f29a18233c9aa191e3c4f585254b04ce8506
benthecoder <benthecoder07@gmail.com>
2023-12-03 19:17:39 -0600

    add beautiful landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,72 @@
-import Scaffold from "./components/Scaffold";
+import React from 'react';
+import { Box, Flex, Heading, Text, Button, Image } from '@chakra-ui/react';
+
+function Hero() {
+  return (
+    <Box>
+      <Heading
+        fontWeight={600}
+        fontSize='6xl'
+        lineHeight={'110%'}
+        marginBottom={12}
+        fontFamily={'serif'}
+      >
+        Cuepal <br />
+        <Text color={'gray.400'} fontSize='3xl' fontFamily='sans-serif'>
+          AI flashcards for students
+        </Text>
+      </Heading>
+      <Button
+        colorScheme='green'
+        variant='outline'
+        bg='green.400'
+        rounded='full'
+        px={6}
+        _hover={{
+          bg: 'green.500',
+          textColor: 'white',
+        }}
+      >
+        Get Started
+      </Button>
+    </Box>
+  );
+}
+
+function AnimatedBlob() {
+  return (
+    <Box className='relative max-w-full'>
+      <Box className='absolute top-24 left-44 w-72 h-72 bg-blue-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob'></Box>
+      <Box className='absolute top-0 -left-0 w-72 h-72 bg-red-400 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob'></Box>
+      <Box className='absolute -bottom-8 left-44 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-2000'></Box>
+      <Box className='absolute -bottom-8 left-10 w-72 h-72 bg-orange-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000'></Box>
+      <div className='relative'>
+        {' '}
+        <Image
+          src='newton.svg'
+          alt='Vector Art'
+          objectFit='cover'
+          boxSize='500px'
+        />
+      </div>
+    </Box>
+  );
+}
 
 export default function Home() {
   return (
-    <Scaffold>
-      <p>Hello</p>
-    </Scaffold>
+    <Flex
+      align='center'
+      justify='space-between'
+      className='min-h-screen max-h-screen'
+      px={32}
+    >
+      <Box width='50%' paddingX={5}>
+        <Hero />
+      </Box>
+      <Box width='50%' paddingX={5}>
+        <AnimatedBlob />
+      </Box>
+    </Flex>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,27 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        blob: 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(10px, -10px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-20px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
