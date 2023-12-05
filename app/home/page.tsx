72102f5a593b10ac4e7a014438a931bca6a4e4e6
benthecoder <benthecoder07@gmail.com>
2023-12-05 10:48:08 -0600

    reactapex chart next.js

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import {
   Box,
   Flex,
@@ -10,26 +10,30 @@ import {
   Button,
   Heading,
   Icon,
-} from "@chakra-ui/react";
-import Scaffold from "../components/Scaffold";
-import { PiCalendar } from "react-icons/pi";
-import ReactApexChart from "react-apexcharts";
+} from '@chakra-ui/react';
+import Scaffold from '../components/Scaffold';
+import { PiCalendar } from 'react-icons/pi';
+
+import dynamic from 'next/dynamic';
+const ReactApexChart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+});
 
 export default function Dashboard() {
   const deadlines = [
-    { id: 1, name: "Newton's Gravity", due: "2021-10-5" },
-    { id: 2, name: "Keppler's Laws", due: "2021-10-10" },
-    { id: 3, name: "The Moon", due: "2021-10-15" },
+    { id: 1, name: "Newton's Gravity", due: '2021-10-5' },
+    { id: 2, name: "Keppler's Laws", due: '2021-10-10' },
+    { id: 3, name: 'The Moon', due: '2021-10-15' },
   ];
 
   const names = [
-    "Tiffany",
-    "George",
-    "Lucas",
-    "Alexandria",
-    "John",
-    "Jane",
-    "Bob",
+    'Tiffany',
+    'George',
+    'Lucas',
+    'Alexandria',
+    'John',
+    'Jane',
+    'Bob',
   ];
   const students = names.map((name, index) => {
     return {
@@ -41,7 +45,7 @@ export default function Dashboard() {
 
   const chartOptions = {
     chart: {
-      type: "bar",
+      type: 'bar',
       height: 350,
     },
     plotOptions: {
@@ -54,118 +58,118 @@ export default function Dashboard() {
       enabled: false,
     },
     xaxis: {
-      categories: ["Quiz 1", "Quiz 2", "Quiz 3", "Quiz 4", "Quiz 5"],
+      categories: ['Quiz 1', 'Quiz 2', 'Quiz 3', 'Quiz 4', 'Quiz 5'],
     },
   };
 
   const chartSeries = [
     {
-      name: "Quiz Scores",
+      name: 'Quiz Scores',
       data: [5, 8, 3, 5, 2],
     },
   ];
 
   return (
     <Scaffold>
-      <Box bg="white" minH="full" borderRadius="lg" shadow="md" m={1} p={5}>
-        <Flex direction="column">
-          <Heading as="h1" size="lg" fontWeight="semibold" mb={4}>
+      <Box bg='white' minH='full' borderRadius='lg' shadow='md' m={1} p={5}>
+        <Flex direction='column'>
+          <Heading as='h1' size='lg' fontWeight='semibold' mb={4}>
             Good morning, Alexandria
           </Heading>
-          <Text color={"gray"}>Are you ready to learn?</Text>
+          <Text color={'gray'}>Are you ready to learn?</Text>
           <Grid
-            templateColumns="1fr 2fr 1fr"
+            templateColumns='1fr 2fr 1fr'
             gap={3}
             px={3}
             py={6}
-            width="full"
+            width='full'
           >
             <Box
-              borderRadius="md"
-              backgroundColor="gray.50"
-              overflowY="scroll"
+              borderRadius='md'
+              backgroundColor='gray.50'
+              overflowY='scroll'
               padding={3}
-              maxH={"calc(100vh - 200px)"}
+              maxH={'calc(100vh - 200px)'}
             >
-              <VStack spacing={4} p={4} overflowY="auto">
+              <VStack spacing={4} p={4} overflowY='auto'>
                 <HStack
-                  justifyContent="space-between"
-                  width="full"
-                  position="sticky"
+                  justifyContent='space-between'
+                  width='full'
+                  position='sticky'
                   top={0}
                   zIndex={1}
                 >
                   <Icon as={PiCalendar} boxSize={10} />
-                  <Text fontSize="2xl" fontWeight="bold">
+                  <Text fontSize='2xl' fontWeight='bold'>
                     Deadlines
                   </Text>
                 </HStack>
                 {deadlines.map((deadline) => (
                   <Box
                     key={deadline.id}
-                    borderWidth="1px"
-                    borderColor={"black"}
-                    backgroundColor={"gray.100"}
-                    borderRadius={"md"}
+                    borderWidth='1px'
+                    borderColor={'black'}
+                    backgroundColor={'gray.100'}
+                    borderRadius={'md'}
                     p={5}
-                    height="100px"
-                    cursor="pointer"
-                    fontSize="xl"
-                    fontWeight={"bold"}
-                    w="full"
+                    height='100px'
+                    cursor='pointer'
+                    fontSize='xl'
+                    fontWeight={'bold'}
+                    w='full'
                   >
                     {deadline.name}
-                    <Text fontSize="sm" fontWeight="normal">
+                    <Text fontSize='sm' fontWeight='normal'>
                       Due: {deadline.due}
                     </Text>
                   </Box>
                 ))}
               </VStack>
             </Box>
-            <Box borderRadius="md" minH="full" padding={3}>
-              <Text fontSize="3xl" fontWeight="bold" mb={4}>
+            <Box borderRadius='md' minH='full' padding={3}>
+              <Text fontSize='3xl' fontWeight='bold' mb={4}>
                 Analytics
               </Text>
               <ReactApexChart
                 options={chartOptions}
                 series={chartSeries}
-                type="bar"
+                type='bar'
                 height={500}
               />
             </Box>
             <Box
-              borderRadius="md"
-              backgroundColor="gray.50"
-              overflowY="scroll"
+              borderRadius='md'
+              backgroundColor='gray.50'
+              overflowY='scroll'
               padding={3}
-              maxH={"calc(100vh - 200px)"}
+              maxH={'calc(100vh - 200px)'}
             >
-              <VStack spacing={4} p={4} overflowY="auto">
-                <HStack justifyContent="space-between" width="full">
-                  <Text fontSize="2xl" fontWeight="bold">
+              <VStack spacing={4} p={4} overflowY='auto'>
+                <HStack justifyContent='space-between' width='full'>
+                  <Text fontSize='2xl' fontWeight='bold'>
                     Students
                   </Text>
                 </HStack>
                 {students.map((student) => (
                   <Box
                     key={student.id}
-                    borderRadius={"lg"}
-                    borderWidth={"1px"}
-                    backgroundColor="white"
+                    borderRadius={'lg'}
+                    borderWidth={'1px'}
+                    backgroundColor='white'
                     p={2}
-                    height="70px"
-                    cursor="pointer"
-                    fontSize="xl"
-                    fontWeight={"bold"}
-                    w="full"
+                    height='70px'
+                    cursor='pointer'
+                    fontSize='xl'
+                    fontWeight={'bold'}
+                    w='full'
                   >
                     <HStack spacing={2} padding={3}>
                       <Avatar
-                        size="sm"
+                        size='sm'
                         name={student.name}
                         src={student.avatar}
                       />
-                      <Text textAlign="left" w="full">
+                      <Text textAlign='left' w='full'>
                         {student.name}
                       </Text>
                     </HStack>
